024f0b85d6951b766710679608022bd750e5c5b6
Johnspeanut <pengqiong2015fall@hotmail.com>
2022-03-07 18:10:56 -0800

    create User class

---
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/User.java
@@ -0,0 +1,30 @@
+package edu.northeastern.cs5500.starterbot.model;
+
+import lombok.Data;
+
+
+public class User {
+    int id;
+    String username;
+    public User(int id, String username){
+        this.id = id;
+        this.username = username;
+    }
+
+    private int getId(){
+        return this.id;
+    }
+
+    private String getUsername(){
+        return this.username;
+    }
+
+    private void setId(int newId){
+        this.id = newId;
+    }
+
+    private void setUsername(String newUsername){
+        this.username = newUsername;
+    }
+    
+}
