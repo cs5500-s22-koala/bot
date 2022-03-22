aaec5bd8232dee567cfdc8d3890eae043e3930db
Johnspeanut <pengqiong2015fall@hotmail.com>
2022-03-21 22:36:46 -0700

    Adding catch command

--- src/main/java/edu/northeastern/cs5500/starterbot/command/CommandModule.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/CommandModule.java
@@ -18,6 +18,7 @@ public class CommandModule {
     public Command provideCatch(CatchCommand catchCommand) {
         return catchCommand;
     }
+
     @Provides
     @IntoSet
     public Command provideFavoriteCommand(FavoriteCommand favoriteCommand) {
