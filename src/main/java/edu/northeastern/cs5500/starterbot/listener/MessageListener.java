3cb0174fa7d071f51226b9c97d91e4d73d77cc13
Johnspeanut <pengqiong2015fall@hotmail.com>
2022-03-28 19:15:37 -0700

    Wecome message

--- src/main/java/edu/northeastern/cs5500/starterbot/listener/MessageListener.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/listener/MessageListener.java
@@ -1,10 +1,14 @@
 package edu.northeastern.cs5500.starterbot.listener;
 
 import edu.northeastern.cs5500.starterbot.command.Command;
+import java.time.OffsetDateTime;
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
@@ -30,5 +34,33 @@ public class MessageListener extends ListenerAdapter {
 
     public Collection<CommandData> allCommandData() {
         return commands.stream().map(Command::getCommandData).collect(Collectors.toList());
+    }
+
+    @Override
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        Member member = event.getMember();
+
+        if ((event.getGuild().getDefaultChannel()) != null) {
+
+            EmbedBuilder builder = new EmbedBuilder();
+
+            builder.setColor(0xf22613);
+            builder.setThumbnail("http://image.png");
+            builder.setTitle("Willkommen auf Da Hood!");
+            builder.setFooter("Powered by Backxtar.");
+            builder.setTimestamp(OffsetDateTime.now());
+            builder.setDescription(
+                    "Herzlich willkommen"
+                            + member.getAsMention()
+                            + "auf **Da Hood**!\n"
+                            + "[**Da Hood - The Best Gaming-Discord!**](https://xyz.gg)");
+
+            member.getUser()
+                    .openPrivateChannel()
+                    .queue(
+                            (ch) -> {
+                                ch.sendMessage(builder.build()).queue();
+                            });
+        }
     }
 }
