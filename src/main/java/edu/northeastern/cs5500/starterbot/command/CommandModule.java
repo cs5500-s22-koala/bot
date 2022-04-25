74231ecf44870cc41767495bc5b050e389ddb032
eudora1126 <liu.yaqia@northeastern.edu>
2022-04-25 10:01:10 -0700

    new edits

--- src/main/java/edu/northeastern/cs5500/starterbot/command/CommandModule.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/CommandModule.java
@@ -12,4 +12,10 @@ public class CommandModule {
     public Command provideSayCommand(SayCommand sayCommand) {
         return sayCommand;
     }
+
+    @Provides
+    @IntoSet
+    public Command provideWelcomeCommand(WelcomeCommand welcomeCommand) {
+        return welcomeCommand;
+    }
 }
