ce5c83564c9559153eb5a5695a2b5e80fa8e88db
Johnspeanut <pengqiong2015fall@hotmail.com>
2022-04-25 20:48:05 -0700

    refactor getrestaurant on zipcode

--- src/main/java/edu/northeastern/cs5500/starterbot/command/GetRestaurantOnZipcodeCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/GetRestaurantOnZipcodeCommand.java
@@ -49,15 +49,17 @@ public class GetRestaurantOnZipcodeCommand implements Command {
             event.reply("No restaurant  found in zipcode " + zipcode).queue();
         } else {
             // TODO: Come up with a better format to display restaurant info
+            StringBuilder sb = new StringBuilder();
             for (Restaurant restaurant : result) {
-                event.reply(
-                                restaurant.getName()
-                                        + " "
-                                        + restaurant.getCuisineType()
-                                        + " "
-                                        + restaurant.getZipcode())
-                        .queue();
+                sb.append(
+                        restaurant.getName()
+                                + " "
+                                + restaurant.getCuisineType()
+                                + " "
+                                + restaurant.getZipcode()
+                                + "\n");
             }
+            event.getChannel().sendMessage(sb.toString()).queue();
         }
     }
 }
