6092e11a26c52b0ddc96ea04de6f13f208130942
eudora1126 <liu.yaqia@northeastern.edu>
2022-04-26 19:14:01 -0700

    yq edit

--- src/main/java/edu/northeastern/cs5500/starterbot/listener/MessageListener.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/listener/MessageListener.java
@@ -5,11 +5,25 @@ import java.util.Collection;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 
 public class MessageListener extends ListenerAdapter {
+    static final String WELCOME_MESSAGE = "Welcome. This is DashBot!";
+
+    /**
+     * Bot send a welcome message to user if it join to a guild.
+     *
+     * @param event GuildJoinEvent, the bot join to a guild.
+     */
+    @Override
+    public void onGuildJoin(GuildJoinEvent event) {
+        TextChannel textChannel = event.getGuild().getDefaultChannel();
+        textChannel.sendMessage(WELCOME_MESSAGE).queue();
+    }
 
     @Inject Set<Command> commands;
 
