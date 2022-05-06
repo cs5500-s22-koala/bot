d74943b38e8c991709dbff0586b8a3dc68ad318d
Johnspeanut <pengqiong2015fall@hotmail.com>
2022-05-06 16:53:59 -0700

    edit more

--- src/test/java/edu/northeastern/cs5500/starterbot/service/MongoDBServiceTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/service/MongoDBServiceTest.java
@@ -3,9 +3,6 @@ package edu.northeastern.cs5500.starterbot.service;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-import edu.northeastern.cs5500.starterbot.model.Restaurant;
-import edu.northeastern.cs5500.starterbot.repository.GenericRepository;
-import edu.northeastern.cs5500.starterbot.repository.MongoDBRepository;
 import org.junit.jupiter.api.Test;
 
 class MongoDBServiceTest {
@@ -15,5 +12,4 @@ class MongoDBServiceTest {
         String URL = MongoDBService.getDatabaseURI();
         assertThat(URL).isNotNull();
     }
-
 }
