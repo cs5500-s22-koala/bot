c2f2b927b24b51e5f0507821a23eb27a1822a5ab
Johnspeanut <pengqiong2015fall@hotmail.com>
2022-05-05 22:06:38 -0700

    customer-and-others

--- src/main/java/edu/northeastern/cs5500/starterbot/command/GetRestaurantOnZipcodeCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/GetRestaurantOnZipcodeCommand.java
@@ -52,19 +52,19 @@ public class GetRestaurantOnZipcodeCommand implements Command {
             String bulletSymbol = ":small_blue_diamond:";
             sb.append(
                     bulletSymbol
-                            + String.format("%20s", "Restaurant Name")
-                            + String.format("%15s", "Cuisine Type")
-                            + String.format("%10s", "Zip Code")
-                            + String.format("%20s", "Operating Hours")
+                            + String.format("%-20s", "Restaurant Name")
+                            + String.format("%-15s", "Cuisine Type")
+                            + String.format("%-10s", "Zip Code")
+                            + String.format("%-20s", "Operating Hours")
                             + String.format("%20s", "Aver.Cost Per Guest")
                             + "\n");
             for (Restaurant restaurant : result) {
                 sb.append(
                         bulletSymbol
-                                + String.format("%20s", restaurant.getName())
-                                + String.format("%15s", restaurant.getCuisineType())
-                                + String.format("%10s", restaurant.getZipcode())
-                                + String.format("%20s", restaurant.getOperatingHours())
+                                + String.format("%-20s", restaurant.getName())
+                                + String.format("%-15s", restaurant.getCuisineType())
+                                + String.format("%-10s", restaurant.getZipcode())
+                                + String.format("%-20s", restaurant.getOperatingHours())
                                 + String.format("%20s", restaurant.getAverageCostPerGuest())
                                 + "\n");
             }
