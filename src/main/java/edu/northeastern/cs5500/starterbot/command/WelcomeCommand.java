9ff26eaae993d54989a47b7374ae0a3b9cb183e2
Johnspeanut <pengqiong2015fall@hotmail.com>
2022-05-06 19:33:23 -0700

    Edit welcome

--- src/main/java/edu/northeastern/cs5500/starterbot/command/WelcomeCommand.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/command/WelcomeCommand.java
@@ -1,5 +1,6 @@
 package edu.northeastern.cs5500.starterbot.command;
 
+import java.awt.Color;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
@@ -27,7 +28,7 @@ public class WelcomeCommand implements Command {
     @NotNull
     @Override
     public CommandData getCommandData() {
-        return null;
+        return new CommandData(getName(), "Welcome");
     }
 
     @Override
@@ -39,6 +40,24 @@ public class WelcomeCommand implements Command {
                         .setDescription("Have a treat")
                         .setImage(
                                 "https://news.cgtn.com/news/3d3d514d3551544d31457a6333566d54/img/d28dd1786f3e4bb598da6e577b369898/d28dd1786f3e4bb598da6e577b369898.jpg")
+                        .setColor(Color.GREEN)
+                        .addField(
+                                "User Commands",
+                                "**/get-restaurant** Get details of a restaurant based on name\n"
+                                        + "**/get-restaurant-zipcode** Get a list of restaurants based on zipcode\n"
+                                        + "**/get-restaurant-zipcode-cuisintype** Get a list of restaurants based on zipcode and cuisine type\n"
+                                        + "**/get-menu-of-restaurant** Show menu of a restaurant\n"
+                                        + "**/place-order** Place an order\n"
+                                        + "**/check-order-status** Check status of an order\n"
+                                        + "**/add-dish-to-cart** Add a dish to shopping cart\n"
+                                        + "**/clear-cart** Clear shopping cart\n",
+                                true)
+                        .addField(
+                                "Admin Commands",
+                                "**/add-restaurant** Add a restaurant into database\n"
+                                        + "**/add-customer** Add a customer into database\n"
+                                        + "**/add-dish** Add a dish into database\n",
+                                true)
                         .build();
         event.replyEmbeds(embedBuilder).queue();
     }
